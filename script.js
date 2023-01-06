3f6aa74bec073fdd768a4220dfc8909d528c7ff4
Eric Tsai <hiphop200199@gmail.com>
2023-01-07 01:41:11 +0800

    持續建構

---
+++ script.js
@@ -0,0 +1,13 @@
+const song1={
+    name:"savage love",
+    bpm:150
+}
+const song2={
+    name:"save your tears",
+    bpm:118
+}
+const song3={
+    name:"blinding lights",
+    bpm:171
+}
+const songs=[song1,song2,song3];
