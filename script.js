f2fb43a36fe2ce62e1d8b896e613549befeb0cb9
Eric Tsai <hiphop200199@gmail.com>
2023-01-07 14:02:00 +0800

    基本mp3架構概念完成

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const song1={
-    name:"savage love",
-    bpm:150
-}
-const song2={
-    name:"save your tears",
-    bpm:118
-}
-const song3={
-    name:"blinding lights",
-    bpm:171
-}
-const songs=[song1,song2,song3];
+document.addEventListener("DOMContentLoaded",()=>{
+    const s1=new Audio("SavageLove.mp3");
+    const song1={
+        name:"Savage Love..",
+        bpm:150
+    }
+    const playButton=document.getElementById("play");
+    let message=document.getElementById("title");
+    let time=document.querySelector(".time");
+    let icon=document.querySelector(".icon");
+    let info=document.getElementById("info");
+playButton.addEventListener("click",()=>{   if(s1.paused){s1.play();
+    message.innerText=song1.name;
+    time.style.animation=`progress ${60/(song1.bpm)*4}s  infinite`;
+    icon.style.animation=`beats ${60/(song1.bpm)*4}s  infinite`;
+    info.innerText=song1.bpm+"BPM..";
+    }else{
+        s1.pause();
+    message.innerText="Where`s your beat?..";
+    time.style.animation="none";
+    icon.style.animation="none";
+    info.innerText="";
+    }
+    
+});
+})
