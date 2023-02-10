f999595a5dc7280d8c86510ef243c2a1db7c169b
Eric Tsai <hiphop200199@gmail.com>
2023-02-10 09:46:25 +0800

    更新播放時間條，可調整到想播放的時間點

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",()=>{
    
     const songs=[{name:"少女",url:"https://github.com/hiphop200199/music-player/blob/main/少女.mp3?raw=true"},{name:"心酸",url:"https://github.com/hiphop200199/music-player/blob/main/心酸.mp3?raw=true"},{name:"成全",url:"https://github.com/hiphop200199/music-player/blob/main/成全.mp3?raw=true"},{name:"伯樂",url:"https://github.com/hiphop200199/music-player/blob/main/伯樂.mp3?raw=true"},{name:"背影",url:"https://github.com/hiphop200199/music-player/blob/main/背影.mp3?raw=true"},{name:"時光腳步聲",url:"https://github.com/hiphop200199/music-player/blob/main/時光腳步聲.mp3?raw=true"},{name:"浪費",url:"https://github.com/hiphop200199/music-player/blob/main/浪費.mp3?raw=true"},{name:"神秘嘉賓",url:"https://github.com/hiphop200199/music-player/blob/main/神秘嘉賓.mp3?raw=true"},{name:"連名帶姓",url:"https://github.com/hiphop200199/music-player/blob/main/連名帶姓.mp3?raw=true"},{name:"想自由",url:"https://github.com/hiphop200199/music-player/blob/main/想自由.mp3?raw=true"},{name:"感同身受",url:"https://github.com/hiphop200199/music-player/blob/main/感同身受.mp3?raw=true"},{name:"說謊",url:"https://github.com/hiphop200199/music-player/blob/main/說謊.mp3?raw=true"},{name:"cinematicMelody",url:"https://github.com/hiphop200199/music-player/blob/main/cinematicMelody.mp3?raw=true"},{name:"FirstLove",url:"https://github.com/hiphop200199/music-player/blob/main/FirstLove.mp3?raw=true"},{name:'LaLaLaLoveSong',url:"https://github.com/hiphop200199/music-player/blob/main/LaLaLaLoveSong.mp3?raw=true"},{name:"Levitating",url:"https://github.com/hiphop200199/music-player/blob/main/Levitating.mp3?raw=true"},{name:"LoveMeLikeYouDo",url:"https://github.com/hiphop200199/music-player/blob/main/LoveMeLikeYouDo.mp3?raw=true"},{name:"pianoMoment",url:"https://github.com/hiphop200199/music-player/blob/main/pianoMoment.mp3?raw=true"},{name:"SaveYourTears",url:"https://github.com/hiphop200199/music-player/blob/main/SaveYourTears.mp3?raw=true"},{name:"Sugar",url:"https://github.com/hiphop200199/music-player/blob/main/Sugar.mp3?raw=true"}];
     let trackIndex=0;
-    let timeUnit;
+    //let timeUnit;
     let player=new Audio(songs[trackIndex]["url"]);
     const whole=document.body;
     const prevBtn=document.getElementById("prev");
@@ -15,8 +15,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     let currentTime=document.querySelector(".current-time");
     let songDuration=document.querySelector(".song-duration");
     let message=document.getElementById("title");
-    let timeBar=document.querySelector(".time");
-    let timeIcon=document.querySelector(".icon");
+    /*let timeBar=document.querySelector(".time");
+    let timeIcon=document.querySelector(".icon");*/
+    let timeRangeBar=document.getElementById("time-bar");
     let convertTime = function(time){    
         let mins = Math.floor(time / 60);
         let secs = Math.floor(time % 60);
@@ -29,18 +30,20 @@ document.addEventListener("DOMContentLoaded",()=>{
     function checkAudioEnded(){
         if(player.ended){
             playBtn.innerHTML=`&#9658`;
-            timeBar.style.width=`0%`;
-            timeIcon.style.left=`0%`;
-            
+            /*/timeBar.style.width=`0%`;
+            timeIcon.style.left=`0%`;*/
         }
         
     }
     function getTime(){
-        timeUnit=100/(Math.floor(player.duration));
+        //timeUnit=100/(Math.floor(player.duration));
         currentTime.innerText=convertTime(player.currentTime);
         songDuration.innerText=convertTime(player.duration);
-        timeBar.style.width=`${timeUnit * player.currentTime}%`;
-        timeIcon.style.left=`${timeUnit * player.currentTime}%`;
+       console.log(timeRangeBar.value);
+        timeRangeBar.setAttribute("max",player.duration);
+        timeRangeBar.value=player.currentTime;
+        /*timeBar.style.width=`${timeUnit * player.currentTime}%`;
+        timeIcon.style.left=`${timeUnit * player.currentTime}%`;*/
         checkAudioEnded();
     };
    let musicInterval=setInterval(getTime,1000);
@@ -79,12 +82,14 @@ playBtn.addEventListener("click",function(){
 nextBtn.addEventListener("click",function(){
       if(trackIndex==songs.length-1){
         player.pause();
+        timeRangeBar.value=0;
         playBtn.innerHTML=`&#9658`;
         trackIndex=0;
         message.innerText=songs[trackIndex]["name"];
         player.src=songs[trackIndex]["url"];
     }else{
     player.pause();
+    timeRangeBar.value=0;
     playBtn.innerHTML=`&#9658`;
     trackIndex+=1;
     message.innerText=songs[trackIndex]["name"];
@@ -95,12 +100,14 @@ prevBtn.addEventListener("click",function(){
     
     if(trackIndex==0){
         player.pause;
+        timeRangeBar.value=0;
         playBtn.innerHTML=`&#9658`;
         trackIndex=songs.length-1;
         message.innerText=songs[trackIndex]["name"];
         player.src=songs[trackIndex]["url"];
     }else{
     player.pause;
+    timeRangeBar.value=0;
     playBtn.innerHTML=`&#9658`;
     trackIndex-=1;
     message.innerText=songs[trackIndex]["name"];
@@ -134,7 +141,11 @@ loopBtn.addEventListener("click",function(){
     loopBtn.style.animation="none";
     }
 });
-
+timeRangeBar.addEventListener("change",function(){
+    playBtn.innerHTML=`&#9658`;
+    player.pause();
+    player.currentTime=timeRangeBar.value;
+});
 
 
 });
